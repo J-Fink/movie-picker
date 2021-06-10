f240753394b73c0970179e5b3598660fe4850fac
J-Fink <57537040+J-Fink@users.noreply.github.com>
2021-06-09 23:15:43 -0500

    update --red variable

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -10,7 +10,7 @@ const GlobalStyles = createGlobalStyle`
     font-style: normal;
   }
   html {
-    --red: #ff0000;
+    --red: #f74d4d;
     --black: #393939;
     --grey: #3A3A3A;
     --gray: var(--grey);
