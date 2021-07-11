7c4024994754e32ee51e974501bc891ea03e78c9
Joe Fink <57537040+J-Fink@users.noreply.github.com>
2021-07-10 23:17:23 -0500

    fix error page on homepage, add signinplease wall

--- frontend/pages/movies/index.js
+++ frontend/pages/movies/index.js
@@ -1,16 +1,17 @@
 import { useRouter } from 'next/dist/client/router';
 import Pagination from '../../components/Pagination'
 import Movies from '../../components/Movies';
+import PleaseSignIn from '../../components/PleaseSignIn';
 
 export default function MoviesPage() {
     const { query }= useRouter();
     const page = parseInt(query.page);
     return (
-        <div>
+        <PleaseSignIn>
             <Pagination page={page || 1} />
             <Movies page={page || 1} />
             <Pagination page={page || 1} />
-        </div>
+        </PleaseSignIn>
     )
   }
   
