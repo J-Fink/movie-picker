19f8e9c2e1ed92cf0eee1554104fd85f5ecb001f
Joe Fink <57537040+J-Fink@users.noreply.github.com>
2021-07-17 15:21:29 -0500

    add about page

---
+++ frontend/pages/about.js
@@ -0,0 +1,14 @@
+import styled from "styled-components"
+
+const AboutPageStyles = styled.div`
+
+`;
+
+export default function AboutPage() {
+    return (
+        <AboutPageStyles>
+            <h1>Welcome to Movie Picker</h1>
+            <div>My wife and I have a hard time choosing something we both want to watch. My solution? Make a computer choose for us</div>
+        </AboutPageStyles>
+    )
+}
