302e423d48132a004a3adb99ef67a053d5faa43e
Joe Fink <57537040+J-Fink@users.noreply.github.com>
2021-08-11 06:27:53 -0500

    center welcome message

--- frontend/pages/about.js
+++ frontend/pages/about.js
@@ -4,6 +4,7 @@ const AboutPageStyles = styled.div`
 border: 4px solid white;
 border-radius: 10px;
 max-width: 400px;
+margin: auto;
 /* margin: var(--headerHeight) auto auto auto; */
 box-shadow: rgb(0 0 0 / 20%) 2px 2px 3px;
 padding-bottom: 5px;
