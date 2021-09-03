1ee91529694a720f285cd4e987aa0457a26f1536
Joe Fink <57537040+J-Fink@users.noreply.github.com>
2021-09-03 16:48:09 -0500

    fix error on initial render of pick page. Change style so movie card is centered

--- frontend/pages/pick.js
+++ frontend/pages/pick.js
@@ -1,36 +1,35 @@
 import Shuffle from '../components/Shuffle';
 import { ALL_MOVIES_QUERY } from '../components/Movies';
 import { useQuery } from '@apollo/client';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Movie from '../components/Movie';
 import styled from 'styled-components';
 const PickStyles = styled.div`
     /* margin: var(--headerHeight) auto; */
     max-width: 400px;
+    margin: auto;
 `;
 
 export default function PickPage() {
+
     const { data, error, loading }  = useQuery(ALL_MOVIES_QUERY);
+    
+    const [seeMovie, setSeeMovie] = useState(false);
+    const [movie, setMovie] = useState(false);
+    
+    const handleClick = () => {
+        // alert(`hello Joe ${data.allMovies[0].name}`);
+        setSeeMovie(true);
+        setMovie(Shuffle([...data.allMovies])[0]);
+    }
     if (loading) return <p>Loading...</p>;
     if (error) return <p>{error.message}</p>
-    const array = Shuffle([...data.allMovies]);
-    console.log(array[0]);
-    const [randomMovie, setRandomMovie] = useState();
-    // function randomMovie() {
-    //     return (
-    //         alert(`here is your movie, ${array[0].name}`)
-    //     )
-    // }
     
-    // const displayMovie = data.allMovies;
-    // console.log(displayMovie);
-    // const displayMovieShuffle = Shuffle();
-    // const displayMovieShuffle = Shuffle(displayMovie);
-    // console.log(displayMovieShuffle);
     return (
         <PickStyles>
-            <Movie movie={array[0]}></Movie>
-            <button onClick={() => setRandomMovie(array[0])} type="button">Pick A Movie For Me!</button>
+    
+        {seeMovie ? <Movie movie={movie} /> : ''}
+            <button onClick={handleClick} type="button">Pick A Movie For Me!</button>
             {/* TODO: Set parameters, included movies they've seen, only ones they haven't seen, etc. */}
         </PickStyles>
     )
